5b3c678f12d5e746104bfb797c469a52f7fbd4f7
thedapgent <nicholas@thedappergentleman.ca>
2026-08-24 20:27:42 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from './pages/About';
 import Services from './pages/Services';
 import Pricing from './pages/Pricing';
 import Gallery from './pages/Gallery';
+import OfTheCloth from './pages/OfTheCloth';
 import Policies from './pages/Policies';
 import Contact from './pages/Contact';
 
@@ -18,6 +19,7 @@ export default function App() {
           <Route path="/services" element={<Services />} />
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/gallery" element={<Gallery />} />
+          <Route path="/of-the-cloth" element={<OfTheCloth />} />
           <Route path="/policies" element={<Policies />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
